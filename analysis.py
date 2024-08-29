67578d724387fd0ac2b8e1100a546c7e42880d8a
Maciej Gryka <maciej@rainforestapp.com>
2024-08-29 18:20:20 +0200

    more strict relevancy checking

--- analysis.py
+++ analysis.py
@@ -1,13 +1,15 @@
 # from enum import Enum
 import json
+from typing import Tuple
 
 import docx
 # from pydantic import BaseModel, Field
+from fasthtml.xtend import is_
 import pandas as pd
 from openai import OpenAI
 from fastcore.parallel import threaded
 
-from common import TaskStatus, Term, TermList, Task, TaskJudgement
+from common import TaskStatus, Term, TermList, Task, TaskRelevancy, TaskJudgement
 from terms_cache import get_terms_data
 
 
@@ -51,7 +53,47 @@ def text_to_terms(text: list[str]) -> TermList:
     term_list = completion.choices[0].message.parsed
     return term_list
 
+
+def is_task_relevant(task: Task, terms: TermList) -> Tuple[bool, bool]:
+    client = OpenAI()
+
+    completion = client.beta.chat.completions.parse(
+        model="gpt-4o-2024-08-06",
+        response_format=TaskRelevancy,
+        messages=[
+            system_message(),
+            {
+                "role": "user",
+                "content": f"""
+                    Is the given task is relevant to the objective of the contract?
+
+                    Terms extracted from the contract:
+                    ```json
+                    {terms.json()}
+                    ```
+
+                    The task is: "{task.description}"
+                """
+            }
+        ]
+    )
+    relevancy = completion.choices[0].message.parsed
+    if relevancy is None:
+        return False, False
+    return relevancy.relevant, relevancy.ambiguous
+
+
 def validate_task(task: Task, terms: TermList) -> TaskJudgement:
+    is_relevant, is_ambiguous = is_task_relevant(task, terms)
+    if not is_relevant:
+        strength = "seems" if is_ambiguous else "is"
+        return TaskJudgement(
+            task=task,
+            related_terms=TermList(terms=[]),
+            explanation=f"The task {strength} not relevant to the main objective of the contract.",
+            ambiguous=is_ambiguous,
+            task_status=TaskStatus.INVALID,
+        )
     client = OpenAI()
 
     completion = client.beta.chat.completions.parse(
@@ -62,7 +104,7 @@ def validate_task(task: Task, terms: TermList) -> TaskJudgement:
             {
                 "role": "user",
                 "content": f"""
-                Review the task below and the provided contract terms and determine whether the task is relevant to the object of the contract. If it is, determine whether it violates any of the terms extracted from the contract. If the task validity is completely clear, mark it accordingly, but if there's any doubt, mark it as ambiguous and guess which validity more likely.
+                Review the task below and the provided contract terms and determine whether it violates any of the terms extracted from the contract. If the task validity is completely clear, mark it accordingly, but if there's any doubt, mark it as ambiguous and guess which validity more likely.
 
                 Terms extracted from the contract:
                 ```json
--- common.py
+++ common.py
@@ -23,10 +23,14 @@ class Task(BaseModel):
     amount: str = Field(description="The $ amount to charge for the task")
 
 
+class TaskRelevancy(BaseModel):
+    contract_objective: str = Field(description="The main objective of the contract")
+    relevant: bool = Field(description="Whether the task is relevant to main objective of the contract")
+    ambiguous: bool = Field(description="Whether the relevancy of the task is ambiguous.")
+
+
 class TaskJudgement(BaseModel):
     task: Task
-    contract_objective: str = Field(description="The main objective of the contract")
-    relevant: bool = Field(description="Whether the task is relevant to the main objective of the contract")
     related_terms: TermList = Field(description="The terms related to the task")
     # ideally we'd use max_length, but OpenAI Structured Outputs don't support it
     explanation: str = Field(description="Short chain-of-thought reasoning for whether the task should be accepted or not.")
